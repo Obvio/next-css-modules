0753528036e38ebc7f852edad994de49cd411b18
amit <amit@contextive.com>
2024-09-15 08:44:22 +0300

    cleanups

--- src/app/other/page.tsx
+++ src/app/other/page.tsx
@@ -2,7 +2,7 @@ import InnerWrapper from "@/components/inner-wrapper";
 import styles from './otherPage.module.css'
 
 export default async function OtherPage(){
-    return <InnerWrapper className={styles.h1}>
-        <h1>Other Page Title</h1>
+    return <InnerWrapper>
+        <h1 className={styles.h1}>Other Page Title</h1>
     </InnerWrapper>
 }
